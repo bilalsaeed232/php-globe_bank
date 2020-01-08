dd153adf3d97191a59e002a1444036fd36326fd6
Bilal Saeed <bilalsaeed232@gmail.com>
2020-01-08 17:50:41 +0100

    ADD: subject show page

--- public/staff/subjects/show.php
+++ public/staff/subjects/show.php
@@ -2,10 +2,10 @@
 
 <?php
 // no need to urldecode as php automatically do it in super global variables
-$id = h($_GET['id']); 
+$id = h($_GET['id']) ?? ''; 
 
 if(is_get_request()) {
-    
+    $subject = find_subject_by_id($id);    
 }else {
     exit("No need to post here...");
 }
@@ -17,7 +17,10 @@ if(is_get_request()) {
 <?php include_once(SHARED_PATH . "staff_header.php"); ?>
 
 <div id="content">
-    <h4>Subject ID: <?php echo $id; ?></h4>
+    <h4>Subject ID: <?php echo $subject['id']; ?></h4>
+    <h4>Menu Name: <?php echo $subject['menu_name']; ?></h4>
+    <h4>Position: <?php echo $subject['position']; ?></h4>
+    <h4>Visible: <?php echo $subject['visible']; ?></h4>
     <br>
     <a href="<?php echo url_for('/staff/subjects/index.php'); ?>">&laquo; Go Back</a>
 </div>
