a65e0f920c27334ac9bb4d13be5e7c3a02527c6f
Bilal Saeed <bilalsaeed232@gmail.com>
2020-01-08 16:23:58 +0100

    ADD: error handling while dbconnect and fetch result set

--- private/database.php
+++ private/database.php
@@ -3,15 +3,25 @@ require_once('db_credentials.php');
 
 function db_connect() {
     $connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
-
+    confirm_db_connect();
     return $connection;
 }
-
 
 function db_disconnect($connection){
     if(isset($connection)) {
         mysqli_close($connection);
     }
 }
+
+
+function confirm_db_connect() {
+    if(mysqli_connect_errno()) {
+        $msg = "Error connecting to database: ";
+        $msg .= mysqli_connect_error() . " (" . mysqli_connect_errno() . ")";
+        exit($msg);
+    }
+}
+
+
 
 ?>
--- private/query_functions.php
+++ private/query_functions.php
@@ -7,7 +7,15 @@ function find_all_subjects() {
     $sql .= "ORDER BY position ASC ";
 
     $result = mysqli_query($db, $sql);
+    confirm_result_set($result);
     return $result;
+}
+
+
+function confirm_result_set($result_set) {
+    if(!$result_set) {
+        exit("Error executing query!");
+    }
 }
 
 ?>
